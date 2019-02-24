5febeb1356d4a5b614f8452afff0739e7a638297
Alistair Fay <alistair.fay@gmail.com>
2019-02-24 17:33:41 +0000

    main looping structure an pretty print output

--- scraper.py
+++ scraper.py
@@ -1,19 +1,58 @@
 import requests
 from bs4 import BeautifulSoup
+import pprint
 
-def get_urls(page):
-    '''given a page, returns all the urls from that page''' 
+def visit_page(page, allowed_domain):
+    '''analyse a page for internal links, external links, static resources''' 
+    internal_urls=[]
+    external_urls=[]
+    static_resources=[]
     request=requests.get(page)
     request_text=request.text
     soup=BeautifulSoup(request_text,"html.parser")
-    hrefs=[]
+    #print(soup)
     for link in soup.find_all('a'):
         href=link.get('href')
-        if not href.startswith("#"):
-            hrefs.append(href)
-    return hrefs
+        if allowed_domain in href:
+            if href not in internal_urls:
+                internal_urls.append(href)
+        else:
+            if not href.startswith("#") and href not in external_urls:
+                external_urls.append(href)
+    for link in soup.find_all('img'):
+        src=link.get('src')
+        if src not in static_resources:
+            static_resources.append(src)
+    page_data={"address":page,"internal_urls":internal_urls,"external_urls":external_urls,"static_resources":static_resources}
+    return page_data
 
-root_url="http://www.claythorn.com"
+root_url="https://www.claythorn.com"
 allowed_domain="claythorn.com"
-hrefs=get_urls(root_url)
-print(hrefs)
+internal_urls=[]
+visited_urls=[]
+site_data=[]
+
+internal_urls.append(root_url)
+while len(internal_urls) > 0:
+    this_page=internal_urls[0]
+    page_data=visit_page(this_page, allowed_domain)
+    site_data.append(page_data)
+    for url in page_data["internal_urls"]:
+        if url not in internal_urls and url not in visited_urls:
+            internal_urls.append(url)
+    internal_urls.remove(this_page)
+    visited_urls.append(this_page)
+
+pp = pprint.PrettyPrinter(indent=2)
+pp.pprint(site_data)
+
+
+
+
+
+#for url in site_urls:
+#    if allowed_domain in url:
+#        #visit page
+#    else:
+#        #update external_urls
+##output sitemap
