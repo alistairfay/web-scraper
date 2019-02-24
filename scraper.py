46c576b0cd44a5d511f01dfc6957e942592362ad
Alistair Fay <alistair.fay@gmail.com>
2019-02-24 13:13:05 +0000

    get_urls function

--- scraper.py
+++ scraper.py
@@ -1,12 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
+def get_urls(page):
+    '''given a page, returns all the urls from that page''' 
+    request=requests.get(page)
+    request_text=request.text
+    soup=BeautifulSoup(request_text,"html.parser")
+    hrefs=[]
+    for link in soup.find_all('a'):
+        href=link.get('href')
+        if not href.startswith("#"):
+            hrefs.append(href)
+    return hrefs
+
 root_url="http://www.claythorn.com"
-request=requests.get(root_url)
-request_text=request.text
-#print(request_text)
-soup=BeautifulSoup(request_text,"html.parser")
-#print(soup.prettify())
-for link in soup.find_all('a'):
-    print(link)
-    #print(link.get('href'))
+allowed_domain="claythorn.com"
+hrefs=get_urls(root_url)
+print(hrefs)
